272a501879c436e7a26d39efbe17735adc369d1f
Suresh <suersh@esrtech.io>
2024-02-06 10:37:20 +0545

    Learning tuple

---
+++ tuple.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let tuple = ("Hello", "Hi", "By");
+    println!("First : {}", tuple.0);
+
+    let tuple1 = ((1, 2, 3, 4), (01, 02, 03, 04));
+    println!("Tuple = {:?} ", tuple1);
+}
