17140de337e80611882d9fcbb7e2ae5a63cb689d
Suresh <suersh@esrtech.io>
2024-02-09 13:28:36 +0545

    Lerning mutable

---
+++ mutable.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let fruits = "Orange";
+    let mut sport = "Football";
+    println!("{} {}", fruits, sport);
+    // fruits = "Apple";
+    sport = "Basketball";
+    println!("{}", sport);
+}
