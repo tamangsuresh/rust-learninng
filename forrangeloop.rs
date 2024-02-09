b345c8ac99aa9b37433e8ee1ba5d352838dcad80
Suresh <suersh@esrtech.io>
2024-02-09 14:20:22 +0545

    forrangeloop

---
+++ forrangeloop.rs
@@ -0,0 +1,9 @@
+fn main() {
+    for n in 1..100 {
+        println!("{}", n);
+    }
+    let arr = ["Hello", "World", "Good", "Bye"];
+    for a in arr.iter() {
+        println!("{}", a)
+    }
+}
